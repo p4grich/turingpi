774f970f1d7819a566d48b1b4e3effea17cef510
Moto Otobai <moto@trident>
2022-07-21 22:35:23 -0700

    cleanup code base

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-image-flashcards.sh: linux shell script to install HypiorOS on cf cards
- 
+Automaton for TP Cluster.
+
+Bash script to image and setup requierments for auto booting images on TPC 
+        
+    image-flashcards.sh: linux shell script to install HypiorOS on cf cards and tune cloud-init for use with ansible
+            
+    argument help:
+      image-flashcards.sh dev=/dev/sda image=hypriotos-rpi-v1.12.3.img ip=10.5.94.51 shortname=master-01 -F
+      image-flashcards.sh dev=/dev/sda image=hypriotos-rpi-v1.12.3.img ip=10.5.94.51 shortname=master-01 -F --copy-cfg
+
+      --debug #  debug
+      -F #  force overwrite image to install fresh image
+      -skip-dd # force skip dd image
+      -copy-cfg #  copy configs only
+
+      Arg: 1
+      dev=/dev/sda #  cf card device
+      
+      image=os.img #  image file
+
+      Arg: 3
+      ip=10.5.94.51 # set fixed address
+
+      Arg: 4
+      shortname=master-01 # set hostname
+
+
+    ansible-playbook -i hosts.yml tpc_cluster-playbook.yml  --limit worker-03   --tags=add-static-address -e "vinet=eth0:1 adhawkip=10.5.94.23" 
+
+MS 1:
+
+    ansible -i hosts.yml all -m ping
--- image-flashcards.sh
+++ image-flashcards.sh
@@ -29,6 +29,11 @@ st
   "
 fi
 
+if [[ $(id -u) == 0 ]]; then
+  echo "Do not run as root or with sudo: FAILED"
+  exit 1
+fi
+
 if [[ ! -f /usr/bin/j2 ]]; then
   echo "Missing /usr/bin/j2, Enter or CTRL-c ro run or quit: apt install j2cli"
   read
@@ -77,21 +82,22 @@ fi
 
 
 function is_mount_check() {
-  local __argvar=$1
-  if [[ -d "/media/$USER/HypriotOS" ]]; then
-    os_found=hypriot
+  local __argvar="$1"
+  if [[ -z "$__argvar" ]]; then
+      echo $"{FUNCNAME[0]}: $__argvar error"
+      exit 99
+  fi
+
+  if [ -d "/media/$USER/HypriotOS" ]; then
+    os_found="hypriot"
     boot_vol="/media/$USER/HypriotOS"
     root_vol="/media/$USER/root"
   fi
 
-  if [[ -d "/media/$USER/boot" ]] && [[ -d "/media/$USER/rootfs" ]]; then
-    os_found=rasp
+  if [ -d "/media/$USER/boot" ] && [ -d "/media/$USER/rootfs" ]; then
+    os_found="rasp"
     boot_vol="/media/$USER/boot"
     root_vol="/media/$USER/rootfs"
-  fi
-
-  if [[ "$__argvar" == "os_check" ]]; then
-     echo $os_found
   fi
 
   if [[ "$__argvar" == "mount_status_check" ]]; then
@@ -99,9 +105,18 @@ function is_mount_check() {
       mount_status=true
     else
       mount_status=false
+      exit 50
     fi
   fi
+
+  if [ "$__argvar" == "os_check" ]; then
+     echo $os_found
+  else
+     os_found=fasle
+  fi
+
 }
+
 
 function un_mount(){
   $(sudo umount ${root_vol})
@@ -113,14 +128,14 @@ function un_mount(){
  
   $(sudo umount ${boot_vol})
   if [[ $? == 0 ]]; then
-    echo "umount ${root_boot} OK"
+    echo "umount ${boot_vol} OK"
   else
-    echo "Error unmounting ${root_boot}"
+    echo "Error unmounting ${boot_vol}"
   fi
 }
 
-is_mount_check mount_status_check
-is_mount_check os_check
+is_mount_check "mount_status_check"
+is_mount_check "os_check"
 
 config_files="config.txt cmdline.txt"
 config_file_path="./roles/bootstrap-core/files"
@@ -128,17 +143,28 @@ template_files="user-data.j2"
 template_file_path="./roles/bootstrap-core/templates"
 template_values="inventory_hostname_short=$shortname ansible_host=$ip"
 touch_files="ssh"
-touch_file_path="$boot_vol"
+touch_file_path="${boot_vol}"
+
 
 function post_files(){
+# pass in command value, then path, then list of filenames
     local __cmdvar="$1"
     local __pathvar="$2"
     local __argvars="${@:3}"
+    if [[ -z "$__cmdvar"  ]] || [[ -z "$__cmdvar=" ]] || [[ -z "$__argvars" ]]; then
+      echo "${FUNCNAME[0]}: $__cmdvar $__cmdvar= $__argvars error"
+      exit 99
+    fi
+    
+    if [ "$mount_status" = fasle ]; then
+      echo "flashcard is not mounted"
+      exit 50
+    fi
 
-    if  [ -d "$__pathvar" ]; then
-      echo "${FUNCNAME[0]} path test: PASSED"
+    if [ -d "$__pathvar" ]; then
+      echo "${FUNCNAME[0]} path test $__pathvar: PASSED"
     else
-      echo "${FUNCNAME[0]} path test: FAILED"
+      echo "${FUNCNAME[0]} path test $__pathvar: FAILED"
       exit 1
     fi
 
@@ -152,17 +178,20 @@ function post_files(){
     if [[ $__cmdvar == "template" ]]; then
       for te in $__argvars; do
         if [[ -f  $__pathvar/$te ]]; then
-          echo "${FUNCNAME[0]} file test: PASSWD"
+          echo "${FUNCNAME[0]} file test: PASSED"
         else
           echo "${FUNCNAME[0]} file test: FAILED"
           exit 1 
         fi 
         save_file=$(basename ${te} .j2)
+        for e in $template_values; do
+          export $e
+        done
         user_data_templated=$(echo $template_values | j2 --format=env $__pathvar/$te)
-        echo $user_data_templated > $boot_vol/$save_file
+        echo "$user_data_templated" > $boot_vol/$save_file
       done
       if [[ $? == 0 ]]; then
-        echo "${FUNCNAME[0]} jinja2 templating: PASSWD"
+        echo "${FUNCNAME[0]} jinja2 templating: PASSED"
       else
         echo "${FUNCNAME[0]} jinja2 templating: FAILED"
         exit 1
@@ -174,7 +203,7 @@ function post_files(){
         cp $__pathvars/$c $boot_vol
       done
       if [[ $? == 0 ]]; then
-        echo "${FUNCNAME[0]} copy config files: PASSWD"
+        echo "${FUNCNAME[0]} copy config files: PASSED"
       else
         echo "${FUNCNAME[0]} copy config files: FAILED"
         exit 1
@@ -182,21 +211,17 @@ function post_files(){
     fi
 }
 
+
+#post_files touch $touch_file_path $touch_files
 function make_post_file() {
-  echo "touch boot files"
-  post_files touch $touch_file_path $touch_files
-
-  echo "template files $template_file_path $template_files"
-  post_files template $template_file_path $template_files
-
-  echo "copying config files"
-  post_files copy $config_file_path $config_files
+  post_files "touch" $touch_file_path $touch_files
+  post_files "template" $template_file_path $template_files
+  post_files "copy" $config_file_path $config_files
 }
 
 
-
 if [[ $@ =~ "--copy-cfg" ]]; then
-  if [[ "$mount_status" == "true" ]]; then
+  if [ "$mount_status" == true ]; then
     make_post_file
     echo "running unmount"
     un_mount
@@ -213,7 +238,7 @@ if [[ $@ =~ "--skip-dd" ]]; then
   skip_dd=true
 fi
 
-if [[ ${skip_dd} == "true" ]]; then
+if [ "$skip_dd" == true ]; then
     echo "skipping dd"
     make_post_file
     un_mount
@@ -225,7 +250,7 @@ if [[ ${skip_dd} == "true" ]]; then
       read pg
       $(/usr/bin/pv ${IMAGE} | sudo dd of=${DEV})
       if [[ $? == 0 ]]; then
-        echo "dd image copy to card: PASSWD"
+        echo "dd image copy to card: PASSED"
       else
         echo "dd image copy to card: FAILED"
         exit 1
@@ -234,21 +259,19 @@ if [[ ${skip_dd} == "true" ]]; then
       echo "already found $os_found"
       exit 1
     fi
-
     un_mount
     echo ""
-    echo ">>> ejected fc cards, please reinsert <<<"
+    echo "ejected fc cards, please reinsert"
     read pg
-    is_mount
-
-    if [[ "$mount_status" == "true" ]]; then
-      make_post_files
+    is_mount_check
+    if [ "$mount_status" == true ]; then
+      make_post_file
       echo "running unmount"
       un_mount
       exit 0
     else
-      echo "mount not found"
-      echo "Copying config files to fresh image: FAILED"
+      echo "Mount not mount"
+      echo "--copy-cfg did not find mount, could not copy files: FAILED"
       exit 1
     fi
     exit 0
